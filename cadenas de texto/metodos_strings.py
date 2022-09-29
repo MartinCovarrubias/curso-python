39d2cbc30a45bd1480eb95cc0707692779a822e8
MartinCovarrubias <covarrubias8080@gmail.com>
2022-09-28 21:59:42 -0500

    fifth commit

---
+++ cadenas de texto/metodos_strings.py
@@ -0,0 +1,19 @@
+#metodos de cadenas de texto
+
+cadena = "Este es un ejemplo de metodos de cadenas de Texto"
+
+#convertir a minusculas metodo lower()
+print(cadena.lower()) #metodo lower() convierte a minusculas toda la cadena
+
+#convertir a mayusculas metodo upper()
+print(cadena.upper()) #metodo upper() convierte a mayusculas toda la cadena
+
+#convertir la primera letra de cada palabra a mayusculas metodo title()
+print(cadena.title()) #metodo title() convierte la primera letra de cada palabra a mayusculas
+
+#convertir la primera letra de la cadena a mayusculas metodo capitalize()
+print(cadena.capitalize()) #metodo capitalize() convierte la primera letra de la cadena a mayusculas
+
+#convertir la primera letra de la cadena a minusculas metodo swapcase()
+print(cadena.swapcase()) #metodo swapcase() convierte la primera letra de la cadena a minusculas
+
