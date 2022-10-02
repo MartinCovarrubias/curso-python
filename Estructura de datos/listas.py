7100ad1ae79983ca698811b55a9ed04738ec03be
MartinCovarrubias <covarrubias8080@gmail.com>
2022-10-01 22:09:27 -0500

    seventeenth commit

---
+++ Estructura de datos/listas.py
@@ -0,0 +1,18 @@
+
+lista = [] # definimos una lista vacia
+
+# pueden ser de cualquier tipo de dato
+
+
+lista2 = [1, 2,  "Hola", "Mundo", "Python"] # definimos una lista con datos
+
+print(type(lista)) # imprimimos el tipo de dato de la lista
+
+print(lista2[0]) # imprimimos el primer elemento de la lista, asi se manda a llamar a un elemento de la lista
+
+print(len(lista2)) # imprimimos la longitud de la lista
+
+print(lista2[-1]) # imprimimos el ultimo elemento de la lista
+
+lista2[0] = 5 # modificamos el primer elemento de la lista, asi se modifica un elemento de la lista
+print(lista2)
