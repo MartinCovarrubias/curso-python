b4c78ecbf4bd38063aa4a2c76e1a6c7372d5723c
MartinCovarrubias <covarrubias8080@gmail.com>
2022-10-02 11:17:25 -0500

    commit number 21

---
+++ Estructura de datos/listas/ejercicio1.py
@@ -0,0 +1,9 @@
+lista = [20,50,"curso","python",3.10]
+print(lista)
+dato1 = int(input("ingrese el numero 1: "))
+dato2 = int(input("ingrese el numero 2: "))
+
+lista.insert(0,dato1)
+lista.insert(1,dato2)
+
+print(lista)
---
+++ Estructura de datos/listas/llenar_lista_vacia.py
@@ -0,0 +1,19 @@
+
+lista1 = [3,"hola","mundo","python",2]
+
+lista2 = [9,"jose","juan","pedro",2]
+
+#suma de listas
+#se puede sumar listas con diferentes tipos de datos
+# solo se puede sumar 
+lista3 = lista1 + lista2
+print(lista3)
+
+#para concatenar una lista se usa , y no el signo +
+
+#! llenar una lista vacia con datos del usuario metodo append
+
+lista = []
+edad = int(input("ingrese su edad: "))
+lista.append(edad) 
+print(lista)
