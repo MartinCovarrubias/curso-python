93a69a687a4ddf7884171e4320b541371dd6a244
MartinCovarrubias <covarrubias8080@gmail.com>
2022-10-01 22:26:46 -0500

    commit number 18

---
+++ Estructura de datos/debanado_listas.py
@@ -0,0 +1,13 @@
+# debanado de listas
+
+lista = [1, 2, 3, 4, 5, 6, "Hola", "Mundo", "Python"]
+
+print(lista[0:5]) # imprime los elementos de la lista desde el indice 0 hasta el indice 5
+
+print(lista[:2]) # imprime los elementos de la lista desde el indice 0 hasta el indice 2
+
+print(lista[2:]) # imprime los elementos de la lista desde el indice 2 hasta el final
+
+print(lista[-5:-1]) # imprime los elementos de la lista desde el indice -5 hasta el indice -1
+
+print(lista[::2]) # imprime los elementos de la lista desde el indice 0 hasta el final de 2 en 2
---
+++ Estructura de datos/metodos_listas.py
@@ -0,0 +1,12 @@
+
+lista = [1, 2, 3, 4, 5, 6, "Hola", "Mundo", "Python"]
+
+# append() agrega un elemento al final de la lista
+# append(ELEMENTO)
+lista.append("Nuevo elemento") # agrega el elemento "Nuevo elemento" al final de la lista
+print(lista)
+
+# insert() agrega un elemento en la posicion indicada
+# insert(posicion, elemento)
+lista.insert(0, "Nuevo elemento2") # agrega el elemento "Nuevo elemento" en la posicion 0
+print(lista)
