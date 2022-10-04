6aef792a81f419d9a9c8c760c079dd75a5111453
MartinCovarrubias <covarrubias8080@gmail.com>
2022-10-03 21:52:00 -0500

    commit  number 27

--- Bucles/ejercicio1.py
+++ Bucles/ejercicio1.py
@@ -1,6 +1,6 @@
 
 numero = int(input("Introduce un número: "))
-i = 1
+i = 0
 while i <= 10:
     print(numero, "x", i , "=", numero *i)
     i += 1
---
+++ Bucles/for.py
@@ -0,0 +1,9 @@
+# el for en python recorre elementos iterables
+# como listas, tuplas, diccionarios, etc
+# igual sirve para recorrer rangos
+
+lista =[1,2,3,4,5,6,7,8,9,10]
+
+for i in lista: # i  toma el valor de cada elemento de la lista 
+    print(i) # imprime el valor de i en cada iteracion
+
