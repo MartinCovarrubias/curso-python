0c3e675b382e1bc06cd9cdc90a759611ec2b7098
MartinCovarrubias <covarrubias8080@gmail.com>
2022-10-03 21:22:46 -0500

    commit number 26

---
+++ Bucles/ejercicio2.py
@@ -0,0 +1,7 @@
+edad = int(input("Introduce tu edad: "))
+
+i = 1
+
+while i <= edad:
+    print("Has cumplido", i, "años")
+    i += 1
---
+++ Bucles/while.py
@@ -0,0 +1,7 @@
+# un bucle siempre tiene que tener un iterador, contador, etc
+
+i = 0
+
+while i < 10:  # mientras que i sea menor que 10
+  i += 1  # incrementa i en 1 
+  print(i) # imprime el valor de i en cada iteración del bucle osea 1,2,3,4,5,6,7,8,9,10
