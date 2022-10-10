917448cece85ed29afca4f8ce1a64afffaf4e311
MartinCovarrubias <covarrubias8080@gmail.com>
2022-10-09 22:28:22 -0500

    commit number 33

---
+++ Funciones/parametros_y_argumentos.py
@@ -0,0 +1,14 @@
+
+#* un parametro es una variable que se va utilizar cuando se crea la funcion
+#* un argumento es el valor que se le pasa a la funcion cuando se llama a la funcion
+
+#? ejemplo de funcion con parametros
+
+def suma(a, b): # a y b son parametros
+    return a + b
+
+#? ejemplo de funcion con argumentos
+
+print(suma(2, 3)) # 2 y 3 son argumentos
+
+#! Usando parametros y argumentos podemos reutilizar funciones
