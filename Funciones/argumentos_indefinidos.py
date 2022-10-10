5a930f295d533fa1c5609c397c83f8e5a7d001d1
MartinCovarrubias <covarrubias8080@gmail.com>
2022-10-09 23:17:48 -0500

    commit number 34

---
+++ Funciones/argumentos_indefinidos.py
@@ -0,0 +1,8 @@
+
+#! como almacena los argumentos en una tupla, lista o diccionario
+#* un argumento indefinido es aquel que no sabemos cuantos valores va a recibir la funcion
+
+def argumento (*num): #? se crea una tupla con los argumentos
+    return num
+print(argumento(12, 13, 14, 15, 16, 17, 18, 19, 20))
+
