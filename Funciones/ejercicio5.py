c4e7080ff93ac0cc4d41cce34591b24aed02302b
MartinCovarrubias <covarrubias8080@gmail.com>
2022-10-09 23:53:35 -0500

    commit number 36

---
+++ Funciones/ejercicio5.py
@@ -0,0 +1,12 @@
+
+def area_cuadrado(base,altura):
+    area = base * altura
+    return area
+
+print (area_cuadrado(5,5))
+
+def area_circulo(radio):
+    area = 3.1416 * radio**2
+    return area
+
+print (area_circulo(10))
